554fcc4727b5fe534bfee3284c2eed4d731a3a1c
Daniel Kim <daniel.d.kim13@gmail.com>
2022-02-02 23:46:42 -0500

    prompt section done

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ const promptManager = () => {
       // destructure
       const { name, id, email, officeNumber } = managerData;
       const manager = new Manager(name, id, email, officeNumber);
-      console.log(manager);
+
       teamArray.push(manager);
     });
 };
@@ -86,88 +86,121 @@ const promptManager = () => {
 // employee prompt function.
 const promptEmployee = () => {
   console.log(`========================================
-    Would you like to add an employee?
+    add an employee
 ========================================`);
 
-  return inquirer.prompt([
-    {
-      type: "list",
-      name: "billet",
-      message: "Select the employee's job title.",
-      choices: ["Engineer", "Intern", "Done"],
-    },
-    {
-      type: "input",
-      name: "name",
-      message: "Enter the employee's name (Required)",
-      validate: (name) => {
-        if (name.length > 0 && isNaN(name)) {
-          return true;
-        } else {
-          console.log("Please enter employee's name!");
-          return false;
-        }
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "billet",
+        message: "Select the employee's job title.",
+        choices: ["Engineer", "Intern"],
       },
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "Enter the employee's identification number (Required)",
-      validate: (id) => {
-        if (id.length > 0 && !isNaN(id)) {
-          return true;
-        } else {
-          console.log("Please enter employee's ID number!");
-          return false;
-        }
+      {
+        type: "input",
+        name: "name",
+        message: "Enter the employee's name (Required)",
+        validate: (name) => {
+          if (name.length > 0 && isNaN(name)) {
+            return true;
+          } else {
+            console.log("Please enter employee's name!");
+            return false;
+          }
+        },
       },
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "Enter the employee's e-mail address(Required)",
-      validate: (email) => {
-        // using regular expression to validate e-mail
-        const regexPattern = /.+@\w+[.].+/;
-        const valid = regexPattern.test(email);
+      {
+        type: "input",
+        name: "id",
+        message: "Enter the employee's identification number (Required)",
+        validate: (id) => {
+          if (id.length > 0 && !isNaN(id)) {
+            return true;
+          } else {
+            console.log("Please enter employee's ID number!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter the employee's e-mail address(Required)",
+        validate: (email) => {
+          // using regular expression to validate e-mail
+          const regexPattern = /.+@\w+[.].+/;
+          const valid = regexPattern.test(email);
 
-        if (email.length > 0 && valid) {
-          return true;
-        } else {
-          console.log("Please enter a valid employee's e-mail address!");
-          return false;
-        }
+          if (email.length > 0 && valid) {
+            return true;
+          } else {
+            console.log("Please enter a valid employee's e-mail address!");
+            return false;
+          }
+        },
       },
-    },
-    {
-      type: "input",
-      name: "github",
-      message: "Enter the engineer's github username.",
-      when: ({ billet }) => billet === "Engineer",
-      validate: (gitHub) => {
-        if (gitHub.length > 0) {
-          return true;
-        } else {
-          console.log("Please enter a valid github username!");
-          return false;
-        }
+      {
+        type: "input",
+        name: "github",
+        message: "Enter the engineer's github username.",
+        when: ({ billet }) => billet === "Engineer",
+        validate: (gitHub) => {
+          if (gitHub.length > 0) {
+            return true;
+          } else {
+            console.log("Please enter a valid github username!");
+            return false;
+          }
+        },
       },
-    },
-    {
-      type: "input",
-      name: "school",
-      message: "Enter the intern's school name.",
-      when: ({ billet }) => billet === "Intern",
-      validate: (school) => {
-        if (school.length > 0) {
-          return true;
-        } else {
-          console.log("Please enter the school name!");
-          return false;
-        }
+      {
+        type: "input",
+        name: "school",
+        message: "Enter the intern's school name.",
+        when: ({ billet }) => billet === "Intern",
+        validate: (school) => {
+          if (school.length > 0) {
+            return true;
+          } else {
+            console.log("Please enter the school name!");
+            return false;
+          }
+        },
       },
-    },
-  ]);
+      {
+        type: "confirm",
+        name: "addEmployee",
+        message: "Additional employee?",
+        default: true,
+      },
+    ])
+    .then((employeeData) => {
+      const { billet, name, id, email, github, school, addEmployee } = employeeData;
+
+      let employee;
+
+      if (billet === "Engineer") {
+        employee = new Engineer(name, id, email, github);
+      }
+
+      if (billet === "Intern") {
+        employee = new Intern(name, id, email, school);
+      }
+
+      teamArray.push(employee);
+
+      if (addEmployee) {
+        return promptEmployee();
+      } else {
+        // generate HTML file.
+        return teamArray;
+      }
+    });
 };
 
-promptManager().then(promptEmployee);
+promptManager()
+  .then(promptEmployee)
+  .then((teamArray) => {
+    writeHTML(teamArray);
+  });
